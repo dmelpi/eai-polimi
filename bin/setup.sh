bb1fc6a55fbe377f4abcc6031e488095efc16971
Diego Melpignano <diego.melpignano@gmail.com>
2022-05-30 13:50:35 +0200

    Adding script to setup HSD on Linux

---
+++ bin/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Setup script for hsdatalog under Linux
+# Must be run as root
+
+cp libhs_datalog.so /usr/lib
+
+cp 30-hsdatalog.rules /etc/udev/rules.d
+
+#create hsdatalog group
+echo "Adding hsdatalog group"
+addgroup hsdatalog
+
+#TODO check if the group already exists
+echo "Adding user to hsdatalog group"
+usermod -aG hsdatalog $USER
+
+#TODO should we restart udev?
+
