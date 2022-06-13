dd6c8541995a87f939be554f9ac62302e81d4392
LeonardoIacussiPoli <103179159+LeonardoIacussiPoli@users.noreply.github.com>
2022-06-13 14:11:16 +0200

    updates

---
+++ ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/STM32CubeIDE/Debug/User/CubeAI/Src/network.d
@@ -0,0 +1,83 @@
+User/CubeAI/Src/network.o: \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network.c \
+ ../../Core/Inc/sysconfig.h ../../Core/Inc/SMPinConfig.h \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network.h \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_config.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform.h \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_data.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform_interface.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/datatypes_network.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_format.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_defines.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_datatypes.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/formats_list.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_math_helpers.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_common.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_convert.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_common.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_common_config.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_list.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_conv2d.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_nl.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_pool.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_custom.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_dense.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_dense_dqnn.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_formats_converters.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_generic.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_lite_graph.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_norm.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_rnn.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_sm.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_iforest.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_svc.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_linearclassifier.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_treeensembleclassifier.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_svmregressor.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_conv2d_dqnn.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_pool_dqnn.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_generic_dqnn.h
+../../Core/Inc/sysconfig.h:
+../../Core/Inc/SMPinConfig.h:
+/Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network.h:
+/Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_config.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform.h:
+/Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_data.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform_interface.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/datatypes_network.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_format.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_defines.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_datatypes.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/formats_list.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_math_helpers.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_common.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_convert.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_common.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_common_config.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_list.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_conv2d.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_nl.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_pool.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_custom.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_dense.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_dense_dqnn.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_formats_converters.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_generic.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_lite_graph.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_norm.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_rnn.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_sm.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_iforest.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_svc.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_linearclassifier.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_treeensembleclassifier.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_ml_svmregressor.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_conv2d_dqnn.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_pool_dqnn.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/layers_generic_dqnn.h:
---
+++ ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/STM32CubeIDE/Debug/User/CubeAI/Src/network_data.d
@@ -0,0 +1,25 @@
+User/CubeAI/Src/network_data.o: \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_data.c \
+ ../../Core/Inc/sysconfig.h ../../Core/Inc/SMPinConfig.h \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_data.h \
+ /Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_config.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform_interface.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/datatypes_network.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_format.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_defines.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_datatypes.h \
+ ../../../../../../Middlewares/ST/STM32_AI_Library/Inc/formats_list.h
+../../Core/Inc/sysconfig.h:
+../../Core/Inc/SMPinConfig.h:
+/Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_data.h:
+/Users/leonardoiacussi/eai-polimi/ai_dpu_stwin_box/Projects/STWIN.box/Applications/AiDpuDemo_prj/X-CUBE-AI/App/network_config.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_platform_interface.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/datatypes_network.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_format.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/ai_datatypes_defines.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/core_datatypes.h:
+../../../../../../Middlewares/ST/STM32_AI_Library/Inc/formats_list.h:
